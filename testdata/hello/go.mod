e8a9968d06fc77e1484f66243519b6644dc6caf0
Dave Day <david.scott.day@gmail.com>
2021-05-10 10:06:53 -0400

    add hello test dir

---
+++ testdata/hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/davidsday/hello
+
+go 1.16
---
+++ testdata/hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/davidsday/hello/lib"
+)
+
+func main() {
+	fmt.Println(lib.Hello())
+}
