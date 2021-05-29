f759daa882e87180c7bf017a80806a3614e71c6a
Dave Day <david.scott.day@gmail.com>
2021-05-28 22:14:30 -0400

    starting to use logger

--- main.go
+++ main.go
@@ -32,6 +32,15 @@ var debug int
 var pluginDir string
 
 func main() {
+	// If the file doesn't exist, create it or append to the file
+	file, err := os.OpenFile("go-tdd.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.SetOutput(file)
+
+	log.Println("Logging initiated.")
 
 	// if runtime.GOOS = 'windows' {
 	// just thinking about portability.....
