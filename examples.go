6f886d6a6f31aef9cd91a5cb9b324ebbc31b407a
Dave Day <david.scott.day@gmail.com>
2021-05-29 15:09:34 -0400

    fix return values

--- examples.go
+++ examples.go
@@ -9,7 +9,7 @@ func exampleError(output string) bool {
 	return CheckRegx(regexExampleFail, output)
 }
 
-func findExampleFunc(pluginDir, exampleFuncDecl, path, ignore string) (filename, linenum, testname string) {
+func findExampleFunc(pluginDir, exampleFuncDecl, path, ignore string) (string, string, string) {
 	cmdLine := fmt.Sprintf("%s/bin/ag --vimgrep -G '.*_test.go' --ignore '%s' '%s' %s", pluginDir, ignore, exampleFuncDecl, path)
 	if debug {
 		log.Printf("In findExampleFunc, cmdLine: %s\n", cmdLine)
