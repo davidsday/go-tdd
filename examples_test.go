822fc59e0efc9e4f7aefb8e00953b26f1d400894
Dave Day <david.scott.day@gmail.com>
2021-05-29 10:03:46 -0400

    mv Example tests to examples_test.go

--- examples_test.go
+++ examples_test.go
@@ -1,9 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"testing"
+)
 
 // Output: What's shakin
 func ExampleTestXXXX() {
 	fmt.Println("Hello, World")
 	// Output: What's shakin
+}
+
+//===========================================================================
+//TestExampleError
+//===========================================================================
+
+//TestExampleError ....
+func TestExampleError(t *testing.T) {
+	input := `{"Time":"2021-05-27T10:05:48.703313416-04:00","Action":"output","Package":"example","Test":"ExampleHW","Output":"--- FAIL: ExampleHW (0.00s)\n"}`
+	jlo := JLObject{}
+	jlo.unmarshal(input)
+
+	got := exampleError(jlo.getOutput())
+	want := true
+	if got != want {
+		t.Errorf("got '%s' want '%s'", strconv.FormatBool(got), strconv.FormatBool(want))
+	}
+}
+
+//===========================================================================
+//TestFindExample
+//===========================================================================
+
+//TestFindExampleFunc ....
+func TestFindExampleFunc(t *testing.T) {
+	exampleFuncDecl := `func ExampleHW`
+	plugDir := `/home/dave/.config/nvim/plugged/go-tdd`
+	path := "/home/dave/sw/go/go-tdd/testdata/example/"
+	curDir, _ := os.Getwd()
+	os.Chdir(path)
+	got1, _, _ := findExampleFunc(plugDir, exampleFuncDecl, ".")
+	os.Chdir(curDir)
+
+	want := `/home/dave/sw/go/go-tdd/example_test.go`
+	if got1 != want {
+		t.Errorf("got '%s' want '%s'", got1, want)
+	}
+}
+
+//===========================================================================
+//TestSplitExampleFuncSearchResults
+//===========================================================================
+
+func TestSplitExampleFuncSearchResults(t *testing.T) {
+	results := `example_test.go:7:1:func ExampleHW() {`
+	want := "example_test.go"
+	got1, _, _ := splitExampleFuncSearchResults(results)
+
+	if !reflect.DeepEqual(got1, want) {
+		t.Errorf("Got: '%s', Want: '%s'", got1, want)
+	}
 }
